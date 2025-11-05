b9bc6fc43363c1cb466f91bf9e6ca065fcd3649d
joaovbeutler <jvbeutler70@gmail.com>
2025-11-04 22:30:34 -0300

    Tab1 Welcome

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,40 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React from 'react';
+import { IonPage, IonContent, IonButton, useIonRouter } from '@ionic/react';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+  const router = useIonRouter(); // Hook pra navegar
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+      <IonContent className="welcome-bg" fullscreen>
+        <div className="welcome-card">
+          <h1 className="logo">
+            Coffee <br />
+            <span>Vibes</span>
+          </h1>
+
+          <h2 className="welcome-title">Welcome!</h2>
+
+          <div className="btn-container">
+            <IonButton
+              expand="block"
+              className="btn-primary"
+              onClick={() => router.push('/tab2', 'forward', 'push')}
+            >
+              CREATE ACCOUNT
+            </IonButton>
+
+            <IonButton
+              expand="block"
+              className="btn-secondary"
+              fill="outline"
+              onClick={() => router.push('/tab3', 'forward', 'push')}
+            >
+              LOGIN
+            </IonButton>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
